3b8d94327e5af7f571a8840a44510cd3a2dec58e
brm-yatec <cesar.yate@brm.com.co>
2016-12-14 15:59:30 -0500

    inicial json

--- server/control.php
+++ server/control.php
@@ -1,32 +1,41 @@
 <?php 
 	
 	ini_set('display_errors', '1');
-	//ini_set('display_errors', 1);
+	ini_set('display_errors', 1);
 	error_reporting(E_ALL);
 	require 'db/requires.php';
+	
+	$error = -1;
+	$data="";
 	
 	if (isset($_POST['control']) && !empty($_POST['control']) ) {
 		
 		$control = $_POST['control'];
 		$General = new General();
+		
 		switch ($control) {
+			
+			//doLogin, comprueba datos de inicio de sesión para el administrador, crea cookie 'login' 
 			case 'doLogin':
 				$user = $_POST['usuario'];	
 				$pass = $_POST['pass'];	
-				if (isset($user) && !empty($user) && !empty($pass)  ) {
-					
-					$usuario = $General->getTotalDatos('VenUsuarioAdmin',array('idUsuarioAdmin','nombre','apellido','email','usuario','contrasena'),array('email'=>$user,'contrasena',$pass));
-					printVar($usuario);
-					if (count($usuario) > 0) {
+				if (isset($user) && !empty($user) && !empty($pass)  ) {	
+					$usuario = $General->getTotalDatos('VenUsuarioAdmin',null,array('email'=>$user,'contrasena'=>$pass) );
+					if ($usuario) {
 						$data = $usuario;
-						$error = false;
+						
+						$error = 1;
+						/*
+							valor de la cookie = $nombre+$apellido+email
+						*/
+						$string = base64_encode($data[0]->nombre.'+'.$data[0]->apellido.'+'.$data[0]->email);
+						setcookie("login", $string, time()+(3600*24*7) );
 					}else{
-						$error = true;
+						$error = 2;
 					}
 				} else {
-					$error = true;
+					$error = 3;
 				}
-				echo json_encode($error);
 			break;
 			
 			case '':
@@ -62,4 +71,8 @@
 				break;
 		}//end switch
 	}//end if 
+
+	$result['data'] = $data;
+	$result['error'] = $error;
+	echo json_encode($result);
 ?>
